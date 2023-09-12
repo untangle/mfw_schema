1f9f71f45212ae7f427f1976df1d345a47bc5491
Austin C <acooke@arista.com>
2023-09-12 16:16:11 +0000

    MFW-3388: Dynamic block list: Adding Enable in schema (#35)
    * Added "enabled" to dynamic_lists and created test
    
    * Change in argument handling

--- v1/policy_manager/validatepolicy.py
+++ v1/policy_manager/validatepolicy.py
@@ -42,22 +42,23 @@ class TestPolicyManager(unittest.TestCase):
         """
         setUpClass runs before all tests. Grabs file information, using some defaults if the environment variables 
         fail for any reason. Then performs the regular jsonschema.validate, to check against the schema. This errors 
-        out, so the follow-up tests won't run
+        out, so any follow-up tests won't run
         """
         current_directory = os.path.dirname(os.path.realpath(__file__))
-        try:
-            json_filename   = os.environ["JSON_FILE"]
-            schema_filename = os.environ["SCHEMA_FILE"]
-        except KeyError:
-            json_filename   = os.path.join(current_directory, cls.JSON_FILENAME_DEFAULT)
-            schema_filename = os.path.join(current_directory, cls.SCHEMA_FILENAME_DEFAULT)
-            print("ERROR: Failed to get filenames from environment variables. Using default filenames:")
-            print("\tjson=" + str(json_filename))
-            print("\tschema=" + str(schema_filename))
-
+        
+        if "JSON_FILE" in os.environ and os.environ["JSON_FILE"] != "":
+            json_filename = os.environ["JSON_FILE"]
+        else:
+            json_filename = os.path.join(current_directory, cls.JSON_FILENAME_DEFAULT)
+            print("WARNING: Using default json filename:" + str(json_filename))
         with open(json_filename, "r") as json_fp:
             cls.json_data = json.load(json_fp)
-        
+            
+        if "SCHEMA_FILE" in os.environ and os.environ["SCHEMA_FILE"] != "":
+            schema_filename = os.environ["SCHEMA_FILE"]
+        else:
+            schema_filename = os.path.join(current_directory, cls.SCHEMA_FILENAME_DEFAULT)
+            print("WARNING: Using default schema filename:" + str(schema_filename))
         with open(schema_filename, "r") as schema_fp:
             schema_data = json.load(schema_fp)
 
