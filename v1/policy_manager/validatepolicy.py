a9f8185b686e2183e68228d09cf8a1e0e3fb2d55
Liviu Nicoara <lnicoara@arista.com>
2023-12-06 16:11:15 +0200

    created schema validator

--- v1/policy_manager/validatepolicy.py
+++ v1/policy_manager/validatepolicy.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 
 import copy
-import json
 import os
 import unittest
 
-from pathlib import Path
-
-import referencing
-import jsonschema
-
-from v1.schema_utils.util import ReferenceRetriever
+from v1.schema_utils.util import SchemaValidator
 
 # The json schema policy fields
 ID_FIELD = "id"
@@ -57,8 +51,7 @@ class TestPolicyManager(unittest.TestCase):
     json_condition_groups = ""
     json_policies       = ""
     warning_dict = {}
-    
-    
+       
     @classmethod
     def setUpClass(cls):
         """
@@ -66,35 +59,14 @@ class TestPolicyManager(unittest.TestCase):
         fail for any reason. Then performs the regular jsonschema.validate, to check against the schema. This errors 
         out, so any follow-up tests won't run
         """
+
         current_directory = os.path.dirname(os.path.realpath(__file__))
+        schema_validator = SchemaValidator(current_directory, cls.JSON_FILENAME_DEFAULT, cls.SCHEMA_FILENAME_DEFAULT)
         
-        if "JSON_FILE" in os.environ and os.environ["JSON_FILE"] != "":
-            json_filename = os.environ["JSON_FILE"]
+        if schema_validator.isValid():
+            cls.json_data = schema_validator.getJsonData()
         else:
-            json_filename = os.path.join(current_directory, cls.JSON_FILENAME_DEFAULT)
-            print("WARNING: Using default json filename:" + str(json_filename))
-        with open(json_filename, "r") as json_fp:
-            cls.json_data = json.load(json_fp)
-            
-        if "SCHEMA_FILE" in os.environ and os.environ["SCHEMA_FILE"] != "":
-            schema_filename = os.environ["SCHEMA_FILE"]
-        else:
-            schema_filename = os.path.join(current_directory, cls.SCHEMA_FILENAME_DEFAULT)
-            print("WARNING: Using default schema filename:" + str(schema_filename))
-        with open(schema_filename, "r") as schema_fp:
-            schema_data = json.load(schema_fp)
-
-        schema_file = Path(schema_filename)
-        retriever = ReferenceRetriever(schema_file.parent.resolve())
-        resource = referencing.Resource.from_contents(schema_data)
-
-        try:
-            # Add the resource to a new registry
-            registry = resource @ referencing.Registry(retrieve=retriever.retrieve)  
-            jsonschema.validate(instance=cls.json_data, schema=resource.contents, registry=registry)
-        except Exception as e:
-            print(e)
-            raise unittest.SkipTest("ERROR: Validation of schema failed. Skipping all tests and printing.")
+            raise unittest.SkipTest("ERROR: Validation of schema failed. Skipping all tests and printing.")            
         
         # Sets class variables in case they need to be accessed after initialization, but before tests
         cls.refresh_jsons()
--- v1/schema_utils/util.py
+++ v1/schema_utils/util.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 import json
+import os
 import pathlib
+import jsonschema
 import referencing
 from urllib.parse import urlparse
+from pathlib import Path
 
 class ReferenceRetriever:
     def __init__(self, root_path):
@@ -14,3 +17,56 @@ class ReferenceRetriever:
         p = pathlib.Path(parsed_uri.path).relative_to("/")
         with open(self.root_path / p) as f:
             return referencing.Resource.from_contents(json.load(f))
+        
+class SchemaValidator:
+
+    json_filename = ""
+    json_data = {}
+    schema_filename = ""
+    schema_data = {}
+    
+    def __init__(self, current_directory, json_filename_default, schema_filename_default):
+        self.__setJsonFileName(current_directory, json_filename_default)
+        self.__readJsonData()
+        self.__setSchemaFilenName(current_directory, schema_filename_default)
+        self.__readSchemaData()
+    
+    def isValid(self):
+        schema_file = Path(self.schema_filename)
+        retriever = ReferenceRetriever(schema_file.parent.resolve())
+        resource = referencing.Resource.from_contents(self.schema_data)
+
+        try:
+            # Add the resource to a new registry
+            registry = resource @ referencing.Registry(retrieve=retriever.retrieve)  
+            jsonschema.validate(instance=self.json_data, schema=resource.contents, registry=registry)
+        except Exception as e:
+            print(e)
+            return False
+        
+        return True
+    
+    def getJsonData(self):
+        return self.json_data
+    
+    def __setJsonFileName(self, current_directory, json_filename_default):
+        if "JSON_FILE" in os.environ and os.environ["JSON_FILE"] != "":
+            self.json_filename = os.environ["JSON_FILE"]
+        else:
+            self.json_filename = os.path.join(current_directory, json_filename_default)
+            print("WARNING: Using default json filename:" + str(self.json_filename))
+    
+    def __readJsonData(self):
+        with open(self.json_filename, "r") as json_fp:
+            self.json_data = json.load(json_fp)
+            
+    def __setSchemaFilenName(self, current_directory, schema_filename_default):
+        if "SCHEMA_FILE" in os.environ and os.environ["SCHEMA_FILE"] != "":
+            self.schema_filename = os.environ["SCHEMA_FILE"]
+        else:
+            self.schema_filename = os.path.join(current_directory, schema_filename_default)
+            print("WARNING: Using default schema filename:" + str(self.schema_filename))
+
+    def __readSchemaData(self):
+        with open(self.schema_filename, "r") as schema_fp:
+            self.schema_data = json.load(schema_fp)
